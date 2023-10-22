3642a09c3d9bddafc4ff71a8a5fc52c5c30e3b23
asus <asus@LAPTOP-5RR1SHUE>
2023-10-23 02:01:04 +0800

    Add project files.

---
+++ PilotonTip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PilotonTip
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double totalbill;
+            double liquorcharge;
+            double tipPercentage;
+            double tip;
+
+            Console.WriteLine("Enter Totalbill: ");
+            totalbill = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter liquorcharge: ");
+            liquorcharge = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter tipPercentage: ");
+            tipPercentage = double.Parse(Console.ReadLine());
+
+            tip = (totalbill - liquorcharge) * tipPercentage;
+
+            Console.WriteLine($"TIP: {tip}");
+        }
+    }
+}
